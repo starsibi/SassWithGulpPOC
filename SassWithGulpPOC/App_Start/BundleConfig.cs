994f84089621b80f54879c854780686ea99376a6
starsibi <starsibi@gmail.com>
2020-09-26 16:05:33 +0530

    Initial changes

--- SassWithGulpPOC/App_Start/BundleConfig.cs
+++ SassWithGulpPOC/App_Start/BundleConfig.cs
@@ -25,6 +25,9 @@ namespace SassWithGulpPOC
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/demo").Include(
+                    "~/Content/CSS/demo.css"));
         }
     }
 }
---
+++ SassWithGulpPOC/gulpfile.js
@@ -0,0 +1,23 @@
+﻿/// <binding />
+const { task, src, dest, series, watch } = require("gulp");
+var sass = require("gulp-sass");
+var sourcemap = require("gulp-sourcemaps")
+
+task("style", function () {
+    return src("Content/SASS/**/*.scss")
+        .pipe(sourcemap.init())
+        .pipe(sass({
+            errLogToConsole: true,
+            outputStyle: "compressed"
+        }))
+        .on("error", console.error.bind(console))
+        .pipe(sourcemap.write('./'))
+        .pipe(dest("./Content/CSS/"))
+
+});
+
+//task("default", series("style"));
+
+task("watch", series("style"), function () {
+    watch("Content/SASS/**/*.scss",["style"]);
+});
